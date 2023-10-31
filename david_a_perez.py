473b3e901ff7cc8b7673c1dc47f6d9b8521b7469
eXzacT <damjan.andelkovic1@gmail.com>
2023-10-31 01:12:51 +0100

    functions now correctly skip indexes

--- david_a_perez.py
+++ david_a_perez.py
@@ -4,7 +4,7 @@ import string
 ascii = {char: ord(char) - 96 for char in string.ascii_lowercase}
 
 
-def david_a_perez(input: string) -> int:
+def process_string_bitwise(input: string) -> int:
     """
     Finds the starting index of the first 14-character slice in a string where all characters are unique.
 
@@ -26,11 +26,12 @@ def david_a_perez(input: string) -> int:
         slice = input[i:i + 14]
         state = 0
 
-        for letter in reversed(slice):
+        for index, letter in enumerate(reversed(slice)):
             alphabet_pos = ascii[letter]
             # This means the character appeared twice because 1 and 1 will return a non 0 value (30/10/2023)
             if state & (1 << alphabet_pos) != 0:
-                i += 1
+                # Subtract from 14 because it's reversed (31/10/2023)
+                i += 14 - index
                 break
             state |= 1 << alphabet_pos
         # If we get to this else it means that the loop finished without encountering a duplicate (30/10/2023)
@@ -41,17 +42,18 @@ def david_a_perez(input: string) -> int:
 
 
 def process_string(input_string):
+    """ Same as the above but not using bitwise operations, rather checking with a set (31/10/2023) """
     i = 0
     while i + 14 <= len(input_string):
         slice = input_string[i:i + 14]
-        state = set()
+        letters_set = set()
 
-        for letter in reversed(slice):
-            if letter in state:
-                i += 1
+        for index, letter in enumerate(reversed(slice)):
+            if letter in letters_set:
+                i += 14 - index
                 break
             else:
-                state.add(letter)
+                letters_set.add(letter)
         else:
             return i
 
@@ -64,7 +66,8 @@ s = ''.join(random.choice(string.ascii_lowercase) for _ in range(100))
 # Ensure there is at least one occurrence where 14 of them are different
 s = s[:50] + string.ascii_lowercase[:14] + s[64:]
 
-position = david_a_perez(s)
+position = process_string_bitwise(s)
+
 if position is not None:
     print("The unique seqence starts at position:", position,
           "\nAnd that sequence is:", s[position:position+14])
